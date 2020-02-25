39e7716f10792bd796e1b826dc3ddf3fd732c3d5
mentalK94 <doingnow94@gmail.com>
2020-02-25 19:33:56 +0900

    STL 라이브러리 사용한 정렬 코드 작성

--- algorithms/STL사용법/STL sort.cpp
+++ algorithms/STL사용법/STL sort.cpp
@@ -1,4 +1,39 @@
 #include <iostream>
 #include <algorithm>
 
+using namespace std;
 
+class Student {
+	public:
+		string name;
+		int score;
+		
+		Student(string name, int score) {
+			this->name = name;
+			this->score = score;
+		}
+		
+		// 'Á¡¼ö°¡ Å« ¼ø¼­'(³»¸²Â÷¼ø) 
+		bool operator <(Student &student) {
+			return this->score > student.score;
+		}
+};
+
+//bool compare(int a, int b) {
+//	return a>b;
+//}
+
+int main (void) {
+	Student students[] = {
+		Student("±èÀ±¼º", 90),
+		Student("À±ÁöÈñ", 84),
+		Student("±¸¸íÈÆ", 92),
+		Student("¹Ú°Ç¿ë", 100),
+		Student("À±ÀçÇÑ", 68),
+	};
+	
+	sort(students, students+5);
+	for(int i=0; i<5; i++) {
+		cout << students[i].name << endl;
+	}
+}

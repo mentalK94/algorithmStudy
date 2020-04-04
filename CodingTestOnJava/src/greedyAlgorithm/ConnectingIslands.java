c5749ad98e5faf1f985146b01dc17cf1b1fc211a
mentalK94 <doingnow94@gmail.com>
2020-04-04 14:09:23 +0900

    프로그래머스-섬 연결하기 문제구현

--- CodingTestOnJava/src/greedyAlgorithm/ConnectingIslands.java
+++ CodingTestOnJava/src/greedyAlgorithm/ConnectingIslands.java
@@ -7,8 +7,8 @@ public class ConnectingIslands {
 
 	public static void main(String[] args) {
 		ConnectingIslandsSolution connectingIslandsSolution = new ConnectingIslandsSolution();
-		int[][] costs = { { 0, 1, 1 }, { 0, 4, 5 }, { 2, 4, 1 }, { 2, 3, 1 }, { 3, 4, 1 } };
-		int n = 5;
+		int[][] costs = { { 0, 1, 1 }, { 0, 2, 2 }, { 1, 2, 5 }, { 1, 3, 1 }, { 2, 3, 8 } };
+		int n = 4;
 		System.out.println(connectingIslandsSolution.solution(n, costs));
 
 	}
@@ -56,12 +56,13 @@ class Island implements Comparable<Island> {
 class ConnectingIslandsSolution {
 	public int solution(int n, int[][] costs) {
 		int answer = 0;
-		int idx;
 		ArrayList<Island> isLands = new ArrayList<>();
-		ArrayList<Island> tempList = new ArrayList<>();
-		ArrayList<Integer> landList = new ArrayList<>();
+		int parent[] = new int[n];
 		
-		
+		for(int i=0; i<n; i++) {
+			parent[i] = i;
+		}
+
 		for(int i=0; i<costs.length; i++) {
 			isLands.add(new Island(costs[i][0], costs[i][1], costs[i][2]));
 		}
@@ -69,62 +70,36 @@ class ConnectingIslandsSolution {
 		Collections.sort(isLands);
 		
 		for(int i=0; i<isLands.size(); i++) {
-			 if(!isContain(isLands.get(i).getX(), isLands.get(i).getY(), landList)) { // 연결되지 않은 경우
-				 
-				 if(landList.contains(isLands.get(i).getX()) || landList.contains(isLands.get(i).getY())) {
-					 if(!landList.contains(isLands.get(i).getX()))
-						 landList.add(isLands.get(i).getX());
-					 if(!landList.contains(isLands.get(i).getY()))
-						 landList.add(isLands.get(i).getY());
-				 } else {
-					 System.out.println(isLands.get(i).getX() + " " + isLands.get(i).getY());
-					 idx = isContainTemp(isLands.get(i).getX(), isLands.get(i).getY(), tempList);
-	 				 
-					 if(idx == -1) {	 
-						 tempList.add(isLands.get(i));						 
-					 } else {
-						 if(!landList.contains(tempList.get(idx).getX()))
-							 landList.add(tempList.get(idx).getX());
-						 if(!landList.contains(tempList.get(idx).getY()))
-							 landList.add(tempList.get(idx).getY());
-						 tempList.remove(idx);
-					 }
-				 }
-				 
-				 answer += isLands.get(i).getCost();
-				 // System.out.println(isLands.get(i).getX() + ", " +isLands.get(i).getY() + ", " +isLands.get(i).getCost());
-			 }
-			 			 
-			 if(isConnect(n, landList))
-				 break;
+			if(!isCycle(parent, isLands.get(i).getX(), isLands.get(i).getY())) {
+				unionParent(parent, isLands.get(i).getX(), isLands.get(i).getY());
+				answer += isLands.get(i).getCost();
+			}
+				
 		}
+
 		
 		return answer;
 	}
 	
-	public boolean isContain(int x, int y, ArrayList<Integer> landList) {
-		if(landList.contains(x) && landList.contains(y)) { return true;}
-		return false;
+	public int getParent(int[] parent, int x) { // 그래프의 부모 노드 확인
+		if(parent[x] == x) return x;
+		return getParent(parent, parent[x]);
 	}
 	
-	public int isContainTemp(int x, int y, ArrayList<Island> tempList) {
-		
-		for(int i=0; i<tempList.size(); i++) {
-			if(tempList.contains(x) || tempList.contains(y)) {
-				return i;
-			}
-		}
-		
-		return -1;
+	public void unionParent(int[] parent, int x, int y) { // 그래프 연결 메소드
+		x = getParent(parent, x);
+		y = getParent(parent, y);
+		if(x<y) 
+			parent[y] = x;
+		else
+			parent[x] = y;
 	}
 	
-	public boolean isConnect(int n,  ArrayList<Integer> landList) {
-		if(landList.size() == n) {return true;}
-		return false;
+	public boolean isCycle(int[] parent, int x, int y) { // 사이클을 형성하는지 확인
+		x = getParent(parent, x);
+		y = getParent(parent, y);
+		if(x == y) return true;
+		else return false;
 	}
-	
-//	public void connect(ArrayList<Island> tempList) {
-//		
-//	}
-	
+
 }
--- CodingTestOnJava/src/greedyAlgorithm/Lifeboat.java
+++ CodingTestOnJava/src/greedyAlgorithm/Lifeboat.java
@@ -1,13 +1,12 @@
 package greedyAlgorithm;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class Lifeboat {
 
 	public static void main(String[] args) {
 		LifeboatSolution lifeboatSolution = new LifeboatSolution();
-		int[] people = { 70, 50, 80, 50 };
+		int[] people = { 40,40,40 };
 		int limit = 100;
 		System.out.println(lifeboatSolution.solution(people, limit));
 	}
@@ -16,57 +15,34 @@ public class Lifeboat {
 class LifeboatSolution {
 	public int solution(int[] people, int limit) {
 		int answer = 0;		
-		int minValue;
-		ArrayList<Integer> peopleWeights = new ArrayList<>();
+		int head, tail;
+        boolean flag = false;
+        
+        Arrays.sort(people);
 		
-		for(int i=0; i<people.length; i++) {
-			
-			peopleWeights.add(people[i]);
-		}
-		
-		Collections.sort(peopleWeights);
-		
-		while(true) {
-			
-			if(peopleWeights.size() == 0) {break;}
-			if(peopleWeights.size() == 1) {answer++; break;}
-						 		
-			minValue = peopleWeights.get(0);
-			int remainSpace = limit-minValue;
-			
-			if(remainSpace < peopleWeights.get(1)) {
-				System.out.println("호출1");
-				peopleWeights.remove(0);
-				answer++; 
+		head = 0;
+		tail = people.length-1;
+		for(int i=tail; i>=0; i--) {			
+			if(people[i] + people[head] <= limit) {
+				tail = i;
+                flag = true;
+				break;
 			}
-			else {
-				while(true) {
-					for(int i=peopleWeights.size()-1; i>=0; i--) {
-						if(remainSpace>=peopleWeights.get(i)) {
-							System.out.println("호출2 : " + peopleWeights.get(i));
-							peopleWeights.remove(i);
-							break;
-						}
-					}
-					
-					if(peopleWeights.size() == 0) {break;}
-					
-					if(remainSpace < peopleWeights.get(0)) {break;}
-					
-					for(int i=0; i<peopleWeights.size(); i++) {
-						System.out.println("호출3 : " + peopleWeights.get(i));
-						if(remainSpace>=peopleWeights.get(i)) {
-							peopleWeights.remove(i);
-							break;
-						}
-					}
-										
-				}
-				answer++;
-			}
-			
-		}
+			answer++;
+		}		
 		
+        if(flag) {
+            while(head<=tail) {
+                if(head == tail) {
+                    answer++; break;
+                }
+                if(people[head] + people[tail] <= limit) {				
+                    answer++;
+                    head++;			
+                } else { answer++;}	
+                tail--;			
+            }
+        }
 		
 		return answer;
 	}

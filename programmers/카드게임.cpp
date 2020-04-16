55bdb4d0232f72f32fe66f5ca43df8c882a03be4
mentalK94 <doingnow94@gmail.com>
2020-04-17 01:03:16 +0900

    프로그래머스 - 카드게임

--- programmers/카드게임.cpp
+++ programmers/카드게임.cpp
@@ -4,86 +4,64 @@
 */
 
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int point = 0;
-int maxPoint = 0;
-int leftMaxPoint = 0;
-vector<int> leftTemp;
-int tempPointer = 0;
+int cachePoint[2000][2000];
+int leftCard[2000];
+int rightCard[2000];
 
-bool compare(int a, int b) {
-	return a>b;
-}
-
-void game(vector<int> left, vector<int> right, int point) {
+int game(int leftIdx, int rightIdx) {
 	
 	// 1. 남은 카드가 없는 경우
-	if(!(left.size() > 0 && right.size() > 0)) {
-		maxPoint = max(point, maxPoint);
-		return;
+	if(leftIdx == (sizeof(leftCard)/sizeof(int)) || rightIdx == (sizeof(rightCard)/sizeof(int))) {		
+		return 0;
 	}
-		
-	// 2. 왼쪽이 오른쪽보다 큰 경우 
-	if(left.front() <= right.front()) {
-		
-		// 2-1. 왼쪽 카드더미중 가장 큰 수보다 오른쪽 카드가 더 큰 경우 -> 이 때는 왼쪽 카드더미만 소진되므로
-		// 양쪽 카드모두를 버려줘야 한다. 
-		left.erase(left.begin());
-		if(leftMaxPoint > right.front()) {									
-			game(left, right, point);			
-		} else {
-			right.erase(right.begin());
-		
-			leftTemp.erase(leftTemp.begin(), leftTemp.end()); 
-			for(int i=0; i<left.size(); i++) {
-				leftTemp.push_back(left[i]);
-			}  
-			
-			sort(leftTemp.begin(), leftTemp.end(), compare);
-			leftMaxPoint = leftTemp[0];
-			game(left, right, point);	
-		}
-		
-	} else { // 3. 왼쪽이 오른쪽보다 작은 경우 -> 오른쪽 버리고 포인트 획득
-		point += right.front();
-		right.erase(right.begin());
-		game(left, right, point);
+	
+	if(cachePoint[leftIdx][rightIdx] != -1) { // 값이 존재하는 경우 
+		return cachePoint[leftIdx][rightIdx];
 	}
+	
+	// 2. 왼쪽이 오른쪽보다 작은 경우 -> 오른쪽 버리고 포인트 획득
+	if(leftCard[leftIdx] > rightCard[rightIdx]) {
+		cachePoint[leftIdx][rightIdx] = game(leftIdx, rightIdx+1) + rightCard[rightIdx];
+	} else { // 3. 왼쪽이 오른쪽보다 큰 경우
+		cachePoint[leftIdx][rightIdx] = max(game(leftIdx+1, rightIdx+1),  game(leftIdx+1, rightIdx));
+	}
+	return cachePoint[leftIdx][rightIdx];
 }
 
-int solution(vector<int> left, vector<int> right) { 
-
+int solution(vector<int> left, vector<int> right) {
+	
 	for(int i=0; i<left.size(); i++) {
-		leftTemp.push_back(left[i]);
-	}  
-	
-	sort(leftTemp.begin(), leftTemp.end(), compare);
-	leftMaxPoint = leftTemp[0];
-	
-	game(left, right, point);
-    
-    return maxPoint;
+		for(int j=0; j<right.size(); j++) {
+			cachePoint[i][j] = -1;
+			leftCard[i] = left[i];
+			rightCard[i] = right[i];
+		}
+	}
+
+    return game(0, 0);
 }
 
 int main(void) {
 	
 	vector<int> left;
 	vector<int> right;
+	int n, num;
 	
-	left.push_back(1);
-	left.push_back(1);
-	left.push_back(1);
-	left.push_back(1);
-	left.push_back(3);
-	right.push_back(2);
-	right.push_back(3);
-	right.push_back(1);
-	right.push_back(1);
-	right.push_back(1);
+	cin >> n;
+	
+	for(int i=0; i<n; i++)  {
+		cin >> num;
+		left.push_back(num);
+	}
+	
+	for(int i=0; i<n; i++)  {
+		cin >> num;
+		right.push_back(num);
+	}
 	
 	cout << solution(left, right) << endl;
 	

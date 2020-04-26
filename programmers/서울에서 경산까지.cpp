fc6fbfce8d35a029de5af713f4bd36c518a0f8f2
mentalK94 <doingnow94@gmail.com>
2020-04-26 18:55:16 +0900

    프로그래머스-서울에서 경산까지 문제풀이

--- programmers/서울에서 경산까지.cpp
+++ programmers/서울에서 경산까지.cpp
@@ -1,67 +1,59 @@
 /*
 ** programmers-서울에서 경산까지.cpp
-** 2020-04-22
+** 2020-04-26
 */
 
 #include <vector>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int memory[100001] = { 0, };
-int size;
+int memory[101][100001] = { 0, };
+bool isAccess[101][100001];
+int travelArr[101][4];
+int cities;
+int times;
 
-int dp(int idx, int K, vector<vector<int> > travel) {
+int dp(int idx, int remainTime) {
 	
-	if(idx >= size || K < 0) {
+	if(remainTime < travelArr[idx][2]) {
+		return -1;
+	}
+	
+	if(idx < 0) {
 		return 0;
 	}
 	
-	if(memory[K] != 0) {
-		return memory[K];
+	if(memory[idx][remainTime] != 0) {
+		return memory[idx][remainTime];
 	}
 	
-	if(K >= travel[idx][0]) { // 도보 경로
-		cout << "도보" << idx << endl;
-//		cout << memory[K] << endl;
-//		cout << K-travel[idx][0] << endl;
-//		cout << endl;
-		
-		memory[K] = max(memory[K], dp(idx+1, K-travel[idx][0], travel) + travel[idx][1]);
-		cout << memory[K] << endl;
+	if((remainTime-travelArr[idx][0]) >= 0 && dp(idx-1, remainTime-travelArr[idx][0]) != -1) { // 도보 경로 이동할 수 있는 경우
+		memory[idx][remainTime] = max(memory[idx][remainTime], dp(idx-1, remainTime-travelArr[idx][0])+travelArr[idx][1]);
 	}
 	
-	if(K >= travel[idx][2]) { // 자전거 경로 
-		cout << "자전거" << idx << endl;
-//		cout << memory[K] << endl;
-//		cout << K-travel[idx][2] << endl;
-//		cout << endl;
-		
-		memory[K] = max(memory[K], dp(idx+1, K-travel[idx][2], travel) + travel[idx][3]);
-		cout << memory[K] << endl;
+	if((remainTime-travelArr[idx][2]) >= 0 && dp(idx-1, remainTime-travelArr[idx][2]) != -1) { // 자전거 경로 이동할 수 있는 경우
+		memory[idx][remainTime] = max(memory[idx][remainTime], dp(idx-1, remainTime-travelArr[idx][2])+travelArr[idx][3]);
 	}
 	
-	return memory[K];
+	if(isAccess[idx][remainTime])
+		return memory[idx][remainTime];
 } 
-
-bool compare(int a, int b) {
-	return a>b;
-}
 
 int solution(int K, vector<vector<int> > travel) {
     int answer = 0;
-    size = travel.size(); // travel 행의 갯수 : 거쳐가는 도시의 개수
+    cities = travel.size(); // travel 행의 갯수 : 거쳐가는 도시의 개수
+    times = K; // 남은 시간 
     
-    answer = dp(0, K, travel);
+    for(int i=0; i<cities; i++) {
+    	travelArr[i][0] = travel[i][0];
+    	travelArr[i][1] = travel[i][1];
+    	travelArr[i][2] = travel[i][2];
+    	travelArr[i][3] = travel[i][3];
+	}
     
-   	sort(memory, memory+100001, compare);
-   	
-//   	for(int i=0; i<10; i++) {
-//   		cout << memory[i] << endl;	
-//	}
+    answer = dp(cities, times);
     
-     cout << answer << endl;
     return answer;
 }
 
@@ -71,45 +63,47 @@ int main(void) {
 	vector<int> a;
 	vector<int> b;
 	vector<int> c;
-	// vector<int> d;
+	vector<int> d;
 	
-	a.push_back(500);
-	a.push_back(200);
-	a.push_back(200);
-	a.push_back(100);
-	b.push_back(800);
-	b.push_back(370);
-	b.push_back(300);
-	b.push_back(120);
-	c.push_back(700);
-	c.push_back(250);
-	c.push_back(300);
-	c.push_back(90);
-	
-//	a.push_back(1000);
-//	a.push_back(2000);
-//	a.push_back(300);
-//	a.push_back(700);
-//	b.push_back(1100);
-//	b.push_back(1900);
-//	b.push_back(400);
-//	b.push_back(900);
-//	c.push_back(900);
-//	c.push_back(1800);
-//	c.push_back(400);
-//	c.push_back(700);
+//	a.push_back(500);
+//	a.push_back(200);
+//	a.push_back(200);
+//	a.push_back(100);
 //	
-//	d.push_back(1200);
-//	d.push_back(2300);
-//	d.push_back(500);
-//	d.push_back(1200);
+//	b.push_back(800);
+//	b.push_back(370);
+//	b.push_back(300);
+//	b.push_back(120);
+//	
+//	c.push_back(700);
+//	c.push_back(250);
+//	c.push_back(300);
+//	c.push_back(90);
+	
+	a.push_back(1000);
+	a.push_back(2000);
+	a.push_back(300);
+	a.push_back(700);
+	b.push_back(1100);
+	b.push_back(1900);
+	b.push_back(400);
+	b.push_back(900);
+	c.push_back(900);
+	c.push_back(1800);
+	c.push_back(400);
+	c.push_back(700);
+	
+	d.push_back(1200);
+	d.push_back(2300);
+	d.push_back(500);
+	d.push_back(1200);
 	
 	travel.push_back(a);
 	travel.push_back(b);
 	travel.push_back(c);
-	// travel.push_back(d);
+	travel.push_back(d);
 
-	solution(1650, travel);
+	solution(3000, travel);
 	
 	return 0;
 }

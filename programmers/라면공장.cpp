f790f248601ff83a168ff775aeee877c5890e7bf
mentalK94 <doingnow94@gmail.com>
2020-05-03 02:44:02 +0900

    programmers-디스크 컨트롤러 문제풀이

---
+++ programmers/라면공장.cpp
@@ -0,0 +1,30 @@
+/*
+** programmers-라면공장.cpp
+** 2020-05-03
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int stock, vector<int> dates, vector<int> supplies, int k) {
+    int answer = 0;
+     
+    while(k>0) {
+    	
+    	
+    	
+    	k--;
+	}
+    
+    
+    
+    return answer;
+}
+
+int main(void) {
+	
+
+	return 0; 
+}

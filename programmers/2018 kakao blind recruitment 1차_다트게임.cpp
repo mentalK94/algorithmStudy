d4e92be7d8a078ae1e5c4a70f85d2f5bd9240ace
mentalK94 <doingnow94@gmail.com>
2020-05-14 18:20:04 +0900

    kakao blind 채용 테스트 1차 - 레벨 1 문제풀이

---
+++ programmers/2018 kakao blind recruitment 1차_다트게임.cpp
@@ -0,0 +1,65 @@
+/*
+** programmers-2018 kakao blind recruitment 1차_다트게임.cpp
+** 2020-05-14
+*/
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int solution(string dartResult) {
+    int answer = 0;
+    int idx = 0;
+	int point[3];
+	int n; 
+    
+    for(int i=0; i<dartResult.length(); i++) {
+    	if(dartResult[i]-'0' >= 0 && dartResult[i]-'0' <= 9) { // 숫자인 경우 
+    		if(dartResult[i+1]-'0' == 0) { // 숫자가 10인 경우 
+    			i++;
+				n = 10;
+			} else { // 숫자가 한 자리수인 경우
+                n = dartResult[i]-'0';
+			}
+			continue;
+		}
+					
+
+		if(dartResult[i] == 'S') { // single
+			cout << "s : " << n << endl;
+			point[idx++] = n;
+			continue;
+		} else if(dartResult[i] == 'D') { // double
+			cout << "d : " << n << endl;
+			point[idx++] = n*n;
+			continue;
+		} else if(dartResult[i] == 'T') { // triple
+			cout << "t : " << n << endl;
+			point[idx++] = n*n*n;
+			continue;
+		}
+		
+		if(dartResult[i] == '*') { // 스타상
+			if(idx > 1) {
+				point[idx-2] = 2*point[idx-2];
+			} 	
+			point[idx-1] = 2*point[idx-1];
+		} else if(dartResult[i] == '#') { // 아차상 
+			point[idx-1] = (-1)*point[idx-1];
+		} 
+	}
+    
+    for(int i=0; i<3; i++) {
+    	cout << point[i] << endl;
+    	answer += point[i];
+	}
+	cout << answer << endl;
+    return answer;
+}
+
+int main(void) {
+	
+	solution("1S2D*3T");
+	return 0;
+}

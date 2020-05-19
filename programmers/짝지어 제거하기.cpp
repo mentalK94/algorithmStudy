a1be491d781f9d421ddc5e853e31e359e291cdab
mentalK94 <doingnow94@gmail.com>
2020-05-19 18:13:15 +0900

    프로그래머스-짝지어 제거하기 문제풀이(수정)

--- programmers/짝지어 제거하기.cpp
+++ programmers/짝지어 제거하기.cpp
@@ -4,6 +4,7 @@
 */
 
 #include <iostream>
+#include <vector>
 #include <string>
 using namespace std;
 
@@ -11,21 +12,23 @@ int solution(string s)
 {
 	vector<char> v;
     int answer = 0;
+    int idx = -1;
 
     for(int i=0; i<s.length(); i++) {
     	v.push_back(s[i]);
-    	
+        idx++;
+    	        
     	if(v.size() >= 2) {
-    		if(v[i] == v[i-1]) {
-    			v.pop();
-    			v.pop();
+    		if(v[idx] == v[idx-1]) {
+    			v.erase(v.begin()+(idx-1), v.begin()+(idx+1));    
+                idx -= 2;
 			}
 		}
 	}
 	
 	if(v.size() == 0) {
 		return 1;
-	}
+	} 
 
     return answer;
 }

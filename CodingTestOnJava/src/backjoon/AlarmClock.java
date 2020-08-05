6dc7565786b8c37d5223b762ff54cc1a27ec7922
mentalK94 <doingnow94@gmail.com>
2020-08-05 17:39:09 +0900

    백준-섬의 갯수 문제풀이

---
+++ CodingTestOnJava/src/backjoon/AlarmClock.java
@@ -0,0 +1,35 @@
+/**
+ * Backjoon - 2884. 알람 시계
+ * AlarmClock.java
+ * @date 2020-08-05
+ * @author hansolKim
+ **/
+
+package backjoon;
+
+import java.util.Scanner;
+
+public class AlarmClock {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		
+		if(m >= 45) {
+			m-=45;
+		} else {
+			m+=15;			
+			if(h>0) {
+				h-=1;
+			} else {
+				h = 23;
+			}
+		}
+		
+		System.out.println(h + " " + m);
+	}
+
+}
---
+++ CodingTestOnJava/src/backjoon/BlackJack.java
@@ -0,0 +1,16 @@
+/**
+ * Backjoon - 7576. 블랙잭
+ * BlackJack.java
+ * @date 2020-08-05
+ * @author hansolKim
+ **/
+
+package backjoon;
+
+public class BlackJack {
+
+	public static void main(String[] args) {
+
+	}
+
+}

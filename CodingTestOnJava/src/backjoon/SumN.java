3040ad02f09342971d45013ed669d6ca840d296b
mentalK94 <doingnow94@gmail.com>
2020-08-05 18:01:09 +0900

    백준 잡다한 문제 풀이

---
+++ CodingTestOnJava/src/backjoon/SumN.java
@@ -0,0 +1,13 @@
+package backjoon;
+
+public class SumN {
+    long sum(int[] a) {
+        long ans = 0;
+        
+        for(int i=0; i<a.length; i++) {
+        	ans+=a[i];
+        }
+        
+        return ans;
+    }
+}

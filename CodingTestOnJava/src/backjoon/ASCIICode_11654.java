c86579498a70ed1260832d36a0cef3064a3797fe
mentalK94 <doingnow94@gmail.com>
2020-08-07 17:45:34 +0900

    백준-아스키 코드 문제풀이

---
+++ CodingTestOnJava/src/backjoon/ASCIICode_11654.java
@@ -0,0 +1,22 @@
+/**
+ * Backjoon - 11654. 아스키 코드
+ * ASCIICode_11654.java
+ * @date 2020-08-07
+ * @author hansolKim
+ **/
+
+package backjoon;
+
+import java.util.Scanner;
+
+public class ASCIICode_11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char c = sc.next().charAt(0);
+		
+		System.out.print((int)c);
+	}
+
+}

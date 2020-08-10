2948a305ebca05edd3506d0007e3af4458500463
mentalK94 <doingnow94@gmail.com>
2020-08-10 16:11:43 +0900

    백준-다디얼, 상수, 단어의 갯수 문제풀이

---
+++ CodingTestOnJava/src/backjoon/CountOfWords_1152.java
@@ -0,0 +1,24 @@
+/**
+ * Backjoon - 1152. 단어의 갯수
+ * CountOfWords_1152.java
+ * @date 2020-08-10
+ * @author hansolKim
+ **/
+
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CountOfWords_1152 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.println(st.countTokens());
+	}
+}
---
+++ CodingTestOnJava/src/backjoon/StudyWords_1157.java
@@ -0,0 +1,52 @@
+/**
+ * Backjoon - 1157. 단어 공부
+ * StudyWords_1157.java
+ * @date 2020-08-10
+ * @author hansolKim
+ **/
+
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StudyWords_1157 {
+
+	private static int[] memory;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		memory = new int[26];
+		
+		String input = br.readLine().toUpperCase();
+		
+		for(int i=0; i<input.length(); i++) {
+			int index = input.charAt(i)-'A';
+			
+			memory[index]+=1;
+		}
+		
+		int max = 0;
+		int maxIndex = 0;;
+		
+		for(int i=0; i<memory.length; i++) {
+			if(max < memory[i]) {
+				max = memory[i];
+				maxIndex = i;
+				continue;
+			}
+			
+			if(max == memory[i]) {
+				maxIndex = -1;
+			}
+		}
+		
+		if(maxIndex == -1) {
+			System.out.println("?");
+		} else {
+			System.out.println((char)(maxIndex+65));
+		}
+	}
+
+}

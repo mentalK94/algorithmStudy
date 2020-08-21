7f898e4e135968948d98a9a67a9e0ac254efe690
mentalK94 <doingnow94@gmail.com>
2020-08-21 19:25:40 +0900

    백준-소수 문제풀이

---
+++ CodingTestOnJava/src/backjoon/PrimeNumber_2581.java
@@ -0,0 +1,49 @@
+/**
+ * Backjoon - 2581. 소수
+ * PrimeNumber_2581.java
+ * @date 2020-08-21
+ * @author hansolKim
+ **/
+
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class PrimeNumber_2581 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		boolean[] isNotPrime = new boolean[10001];
+		isNotPrime[1] = true; // 소수가 아님을 표기 		
+		
+		for(int i=2; (i*i)<isNotPrime.length; i++) {
+			for(int j=(i*i); j<isNotPrime.length; j+=i) {
+				isNotPrime[j] = true;
+			}
+		}
+		
+		int M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int min = 0;
+		int sum = 0;
+		
+		for(int i=M; i<=N; i++) {
+			if(!isNotPrime[i]) {
+				if(min == 0) {min=i;}
+				sum+=i;
+			}
+		}
+		
+		if(min == 0)
+			System.out.println(-1);
+		else 
+			System.out.println(sum + "\n" + min);
+	}
+
+}

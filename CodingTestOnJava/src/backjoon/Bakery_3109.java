ee4c7826e88dd0227c0e3adbd56fa648010b4869
mentalK94 <doingnow94@gmail.com>
2020-08-27 15:29:13 +0900

    백준-빵집 문제풀이

--- CodingTestOnJava/src/backjoon/Bakery_3109.java
+++ CodingTestOnJava/src/backjoon/Bakery_3109.java
@@ -13,9 +13,8 @@ import java.util.StringTokenizer;
 
 public class Bakery_3109 {
 
-	private static int[][] map;
+	private static char[][] map;
 	private static int[] dx = { -1, 0, 1 };
-	private static int[] dy = { 1, 1, 1 };
 	private static int R, C; // R : 행 수, C : 열 수
 	private static int answer = 0;
 
@@ -27,63 +26,39 @@ public class Bakery_3109 {
 		R = Integer.parseInt(init.nextToken());
 		C = Integer.parseInt(init.nextToken());
 
-		map = new int[R][C];
+		map = new char[R][];
 
 		for (int i = 0; i < R; i++) {
-
-			String[] data = br.readLine().split("");
-
-			for (int j = 0; j < C; j++) {
-				if (data[j].equals(".")) {
-					map[i][j] = 0;
-				} else { // 건물이 있는 경우
-					map[i][j] = 1;
-				}
-			}
+			map[i] = br.readLine().toCharArray();
 		}
-
-		int step = 2;
 
 		for (int i = 0; i < R; i++) {
-			getPipeline(i, 0, step++);
+			setPipeline(i, 0);
 		}
-
-		for (int t = 0; t < R; t++) {
-			for (int j = 0; j < C; j++) {
-				System.out.print(map[t][j]);
-			}
-			System.out.println();
-		}
-		System.out.println();
 
 		System.out.println(answer);
 	}
 
-	private static void getPipeline(int x, int y, int step) {
+	private static boolean setPipeline(int x, int y) {
 
 		// 1. 유도조건 ---> 맨 오른쪽인 식당에 도착한 경우
 		if (y == C - 1) {
-			System.out.println(x + " " + y);
 			answer++;
-			return;
+			return true;
 		}
 
 		// 2. 기저조건
 		for (int i = 0; i < 3; i++) {
 			int rx = x + dx[i];
-			int ry = y + dy[i];
 
-			if (rx < 0 || rx >= R) { // ry는 검사할 필요가 없다.
-				continue;
-			}
+			if (rx < 0 || rx >= R || map[rx][y+1] != '.') continue; 
 
-			if (map[rx][ry] == 0) { // 갈 수 있는 곳인 경우
-				map[rx][ry] = step;
-				getPipeline(rx, ry, step);
-				break;
-			}
-
+			map[rx][y+1] = '#'; // 파이프 놓기
+			if(setPipeline(rx, y+1)) return true;  // 다음 칸으로 이동 후 진행 결과과 끝까지 연결이 가능했다면 현위치에서 다른 방향으로 파이프 놓기 시도를 중단하고 이전 위치로 돌아감
+			
 		}
+		
+		return false;
 	}
 
 }

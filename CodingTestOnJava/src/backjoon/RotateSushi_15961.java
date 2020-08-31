d58c18e55b9f1c2eecd07d4fc3f007aa3dcb3a39
mentalK94 <doingnow94@gmail.com>
2020-08-31 09:25:29 +0900

    백준-분해합 문제풀이

--- CodingTestOnJava/src/backjoon/RotateSushi_15961.java
+++ CodingTestOnJava/src/backjoon/RotateSushi_15961.java
@@ -10,11 +10,7 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 //최대로 먹는 경우 총 k+1개의 초밥을 먹는다.

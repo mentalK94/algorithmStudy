86c4c45f32abad8b65f3ba724d2fb6f571041a0b
mentalK94 <doingnow94@gmail.com>
2020-09-15 00:05:41 +0900

    백준-덩치 문제풀이

--- CodingTestOnJava/src/backjoon/BigSize_7568.java
+++ CodingTestOnJava/src/backjoon/BigSize_7568.java
@@ -10,48 +10,7 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
-
-class Person implements Comparable<Person> {
-	int weight;
-	int height;
-	int rank;
-	int idx;
-
-	public Person(int weight, int height, int idx) {
-		this.weight = weight;
-		this.height = height;
-		this.idx = idx;
-		this.rank = 0;
-	}
-
-	@Override
-	/* 몸무게 순으로 정렬 */
-	public int compareTo(Person o) {
-		return Integer.compare(o.weight, this.weight);
-	}
-
-	public void setRank(int rank) {
-		this.rank = rank;
-	}
-
-	public int getIdx() {
-		return idx;
-	}
-
-	public int getWeight() {
-		return weight;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public int getRank() {
-		return rank;
-	}
-}
 
 public class BigSize_7568 {
 
@@ -59,53 +18,30 @@ public class BigSize_7568 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		ArrayList<Person> personList = new ArrayList<>();
+		ArrayList<int[]> personList = new ArrayList<>();
 
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
-			personList.add(new Person(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), i));
+			personList.add(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
 		}
-
-		Collections.sort(personList);
-
-		int count = 1;
-		personList.get(0).setRank(1);
-
-		/* rank 셋팅 */
-		for (int i = 0; i < N; i++) {
-		
-			for (int j = 1; j < N; j++) {
-
-				if (personList.get(i).getWeight() == personList.get(j).getWeight()) { // 기준인 사람과 몸무게가 같으면 같은 순위
-					if(personList.get(j).getRank() == 0) {
-						count++;
-					}
-					personList.get(j).setRank(personList.get(i).getRank());
-					continue;
-				} 
-				
-				System.out.println(personList.get(i).getHeight() + " " + personList.get(j).getHeight());
-				if (personList.get(i).getHeight() <= personList.get(j).getHeight()) { // 기준인 사람보다 키가 크거나 같으면
-					if(personList.get(j).getRank() == 0) {
-						count++;
-					}
-					personList.get(j).setRank(personList.get(i).getRank());
-				} else {
-					personList.get(j).setRank(personList.get(i).getRank()+1);
-				}
-			}
-		}
-
-		Collections.sort(personList, (Person p1, Person p2) -> Integer.compare(p1.getIdx(), p2.getIdx()));
 
 		StringBuilder sb = new StringBuilder();
-
-		for (int i = 0; i < N; i++) {
-			sb.append(personList.get(i).getRank()).append(" ");
+		for(int i=0; i<personList.size(); i++) {
+			int cnt = 1;
+			int w = personList.get(i)[0];
+			int h = personList.get(i)[1];
+			
+			// 본인보다 덩치가 큰 사람 몇명있는지 조사
+			for(int j=0; j<personList.size(); j++) {
+				if(w<personList.get(j)[0] && h<personList.get(j)[1]) {
+					cnt++;
+				}
+			}
+			sb.append(cnt).append(" ");
 		}
 
 		System.out.println(sb);
+		br.close();
 	}
 
 }
---
+++ CodingTestOnJava/src/backjoon/DistributedProcessing_1009.java
@@ -0,0 +1,39 @@
+/**
+ * Backjoon - 1009. 분산처리
+ * DistributedProcessing_1009.java
+ * @date 2020-09-13
+ * @author hansolKim
+ **/
+
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class DistributedProcessing_1009 {
+
+	public static void main(String[] args) throws Exception {
+		
+		int[][] memory = {{10}, {1}, {6,2,4,8}, {1,3,9,7}, {6,4},
+						  {5}, {6}, {1,7,9,3}, {6,8,4,2}, {1,9}};
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(a>=10) {
+				a %= 10;
+			}
+			
+			sb.append(memory[a][(b%memory[a].length)]).append("\n");
+		}
+		System.out.println(sb);		
+	}
+
+}

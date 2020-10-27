401ccf19bc7985222818b3f4b3b978c8f52db0e3
김한솔 <doingnow94@gmail.com>
2020-10-27 16:46:55 +0900

    백준-창고 다각형 문제풀이

--- CodingTestOnJava/src/backjoon/WarehousePolygon_2304.java
+++ CodingTestOnJava/src/backjoon/WarehousePolygon_2304.java
@@ -1,7 +1,7 @@
 /**
  * Backjoon - 2304. 창고 다각형
  * WarehousePolygon_2304.java
- * @date 2020-09-22
+ * @date 2020-10-27
  * @author hansolKim
  **/
 
@@ -9,7 +9,6 @@ package backjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class WarehousePolygon_2304 {
@@ -18,44 +17,40 @@ public class WarehousePolygon_2304 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		int[][] pole = new int[N][2];
+		int[] map = new int[1001];
+		int left = 1000;
+		int right = 1;
 		
 		for(int i=0; i<N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());	
-			pole[i][0] = Integer.parseInt(st.nextToken());
-			pole[i][1] = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int height = Integer.parseInt(st.nextToken());
+			map[x] = height;
+			if(x < left) { left = x;}
+			if(x > right) { right = x;}
 		}
 		
-		int maxHeight = 0;
-		int maxIdx = 0;
-		int maxCnt = 0;
+		// 왼쪽, 오른쪽에서 시작해서 면적 저장
+		int[] leftMap = new int[right+1];
+		int[] rightMap = new int[right+2];
 		
-		for(int i=0; i<N; i++) {
-			if(maxHeight < pole[i][1]) {
-				maxHeight = pole[i][1];
-				maxCnt = 1;
-				maxIdx = i;
-			} else if(maxHeight == pole[i][1]) {
-				maxCnt++;
-			}
+		// 왼쪽에서 시작한 면적 구하기
+		for(int i=left; i<=right; i++) {
+			// 이전 영역보다 크면 영역의 크기 변경
+			leftMap[i] = Math.max(leftMap[i-1], map[i]);
 		}
 		
-		// 기둥의 위치를 기준으로 오름차순 정렬
-		Arrays.sort(pole, (o1, o2) -> {
-			return Integer.compare(o1[0], o2[0]);
-		});
-		
-		int area = 0; // 총 면적
-		int curIdx = 0; // 현재 위치		
-		
-		for(int i=0; i<N; i++) {
-			
-			// 1. 현재 기둥의 높이 조사
-			int height = pole[i][1];
-			
-			
+		// 오른쪽에서 시작한 면적 구하기
+		for(int i=right; i>=left; i--) {
+			rightMap[i] = Math.max(rightMap[i+1], map[i]);
 		}
 		
+		int answer = 0;
+		for(int i=left; i<=right; i++) {
+			answer += Math.min(leftMap[i], rightMap[i]);
+		}
+		
+		System.out.println(answer);
 	}
 
 }

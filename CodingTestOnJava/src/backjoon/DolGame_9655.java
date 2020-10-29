7653bd703986321f42bd90b5d2bc0d5f740d1910
김한솔 <doingnow94@gmail.com>
2020-10-29 19:04:47 +0900

    백준-스타트와 링크 문제풀이

---
+++ CodingTestOnJava/src/backjoon/DolGame_9655.java
@@ -0,0 +1,26 @@
+/**
+ * Backjoon - 9655. 돌 게임
+ * DolGame_9655.java
+ * @date 2020-10-29
+ * @author hansolKim
+ **/
+
+package backjoon;
+
+import java.util.Scanner;
+
+public class DolGame_9655 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		N %= 4;
+		
+		if(N==2) {
+			System.out.println("SK");
+		} else {
+			System.out.println("CY");
+		}
+	}
+}
